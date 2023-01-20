095650bdf83119a2a7d10d0b02b43221a8db0a35
Yifan Zhao <stopire@gmail.com>
2023-01-21 03:15:14 +0800

    finish part_a

--- pagerank/page_rank.cpp
+++ pagerank/page_rank.cpp
@@ -1,12 +1,15 @@
 #include "page_rank.h"
 
+#include <cstring>
 #include <stdlib.h>
 #include <cmath>
 #include <omp.h>
 #include <utility>
+#include <vector>
 
 #include "../common/CycleTimer.h"
 #include "../common/graph.h"
+#include "common/graph_internal.h"
 
 
 // pageRank --
@@ -22,13 +25,6 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
 
   // initialize vertex weights to uniform probability. Double
   // precision scores are used to avoid underflow for large graphs
-
-  int numNodes = num_nodes(g);
-  double equal_prob = 1.0 / numNodes;
-  for (int i = 0; i < numNodes; ++i) {
-    solution[i] = equal_prob;
-  }
-  
   
   /*
      CS149 students: Implement the page rank algorithm here.  You
@@ -58,4 +54,54 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
      }
 
    */
+
+  int numNodes = num_nodes(g);
+  double equal_prob = 1.0 / numNodes;
+
+  bool converged = false;
+  double *score_old = (double *)malloc(sizeof(double) * g->num_nodes);
+  double *score_new = (double *)malloc(sizeof(double) * g->num_nodes);
+  std::vector<Vertex> no_outgoing_vertex;
+
+  for (Vertex i = 0; i < numNodes; ++i) {
+    score_old[i] = equal_prob;
+    if (outgoing_size(g, i) == 0) {
+      no_outgoing_vertex.push_back(i);
+    }
+  }
+
+  while (!converged) {
+
+    double no_outgoing_sum = 0.0;
+#pragma omp parallel for reduction(+ : no_outgoing_sum)
+    for (Vertex &v : no_outgoing_vertex) {
+      no_outgoing_sum += damping * score_old[v] / numNodes;
+    }
+
+#pragma omp parallel for
+    for (Vertex vi = 0; vi < numNodes; vi++) {
+      score_new[vi] = 0.0;
+      const Vertex *beg = incoming_begin(g, vi), *end = incoming_end(g, vi);
+      for (const Vertex *vj = beg; vj != end; vj++) {
+        score_new[vi] += score_old[*vj] / outgoing_size(g, *vj);
+      }
+
+      score_new[vi] = (damping * score_new[vi]) + (1.0 - damping) / numNodes;
+
+      score_new[vi] += no_outgoing_sum;
+    }
+
+    double global_diff = 0.0;
+#pragma omp parallel for reduction(+ : global_diff)
+    for (Vertex vi = 0; vi < numNodes; vi++) {
+      global_diff += std::fabs(score_new[vi] - score_old[vi]);
+    }
+    converged = (global_diff < convergence);
+
+    memcpy(score_old, score_new, sizeof(double) * g->num_nodes);
+  }
+
+  memcpy(solution, score_new, sizeof(double) * g->num_nodes);
+  free(score_old);
+  free(score_new);
 }
