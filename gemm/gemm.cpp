6763d5dbaf3b8f6a6962afbfbc49c19b0dc3285b
Yifan Zhao <stopire@gmail.com>
2023-01-22 23:59:58 +0800

    use avx to implement gemm

--- gemm/gemm.cpp
+++ gemm/gemm.cpp
@@ -1,3 +1,4 @@
+#include <immintrin.h>
 
 // gemm -- general double precision dense matrix-matrix multiplication.
 //
@@ -8,21 +9,36 @@
 //
 // Your implementation should make no assumptions about the values contained in any input parameters.
 
+/* caller must ensure: m == n == k && n is power of 2 */
 void gemm(int m, int n, int k, double *A, double *B, double *C, double alpha, double beta){
+  /* 256 / sizeof(double) == 4 */
+  const int step = 4;
 
-    // REPLACE THIS WITH YOUR IMPLEMENTATION
+  __m256d vec1, vec2, res;
+  __m256d vec_alpha = _mm256_set1_pd(alpha);
+  __m256d vec_beta = _mm256_set1_pd(beta);
 
-    int i, j, kk;
-    for (i=0; i<m; i++){
-        for (j=0; j<n; j++){
-	    double inner_prod = 0;
-	    for (kk=0; kk<k; kk++){
-	        inner_prod += A[i*k+kk] * B[kk*n+j];
-	    }
-	    C[i*n+j] = alpha * inner_prod + beta * C[i*n+j];
-	}
+  // C = beta * C
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j += step) {
+      vec1 = _mm256_loadu_pd(&C[i * n + j]);
+      vec1 = _mm256_mul_pd(vec1, vec_beta);
+      _mm256_storeu_pd(&C[i * n + j], vec1);
     }
-    
-    // END OF NAIVE IMPLEMENTATION
+  }
+
+  // C += alpha * A x B
+  for (int i = 0; i < n; i++) {
+    for (int k = 0; k < n; k++) {
+      vec1 = _mm256_set1_pd(A[i * n + k]);
+      for (int j = 0; j < n; j += step) {
+        vec2 = _mm256_loadu_pd(&B[k * n + j]);
+        res =
+            _mm256_add_pd(_mm256_loadu_pd(&C[i * n + j]),
+                          _mm256_mul_pd(vec_alpha, _mm256_mul_pd(vec1, vec2)));
+        _mm256_storeu_pd(&C[i * n + j], res);
+      }
+    }
+  }
 }
 
